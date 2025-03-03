0e5abd57e4fa4e49795889c0360910eefc25dea6
Xuan-Loc <xuanloc060393@gmail.com>
2025-03-03 19:58:57 +0700

    03/03/2025 - Start

---
+++ HOME_FRONT_END/src/Admin/AdminLayout.jsx
@@ -0,0 +1,66 @@
+import {Layout, Menu} from 'antd';
+import classname from 'classnames/bind'
+import {
+    AppstoreOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    ShopOutlined,
+    TeamOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+
+import style from '../Admin/AdminLayout.module.scss'
+import React from 'react';
+
+const {Header, Content, Sider} = Layout
+const cx = classname.bind(style)
+const items = [
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    AppstoreOutlined,
+    TeamOutlined,
+    ShopOutlined,
+  ].map((icon, index) => ({
+    key: String(index + 1),
+    icon: React.createElement(icon),
+    label: `nav ${index + 1}`,
+  }));
+
+function AdminLayout(){
+    return(
+        <Layout hasSider>
+            <Sider className={cx('sider')}>Sider</Sider>
+            <Layout>
+                <Header className={cx('header')}>
+                    <div className={cx('header-children')}>Header</div>
+                </Header>
+                <Content className={cx('content')}>
+                    <div className={cx('content-children')}>
+                        <p>long content</p>
+                        {
+                        // indicates very long content
+                        Array.from(
+                            {
+                            length: 100,
+                            },
+                            (_, index) => (
+                            <React.Fragment key={index}>
+                                {index % 20 === 0 && index ? 'more' : '...'}
+                                <br />
+                            </React.Fragment>
+                            ),
+                        )
+                        }
+                    </div>
+                </Content>
+            </Layout>
+        </Layout>
+    )
+}
+
+export default  AdminLayout;
