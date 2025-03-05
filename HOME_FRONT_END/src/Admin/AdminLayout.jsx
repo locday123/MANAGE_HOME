ef862799653ce726bdf8bbb4ae7c03f103cd26bf
Xuan-Loc <xuanloc060393@gmail.com>
2025-03-05 19:47:43 +0700

    05/03/2025 - Start 2

--- HOME_FRONT_END/src/Admin/AdminLayout.jsx
+++ HOME_FRONT_END/src/Admin/AdminLayout.jsx
@@ -1,47 +1,53 @@
-import { Avatar, Dropdown, Layout, Space } from "antd";
-import classname from "classnames/bind";
+import {Avatar, Dropdown, Layout, Space, theme} from "antd"
+import classname from "classnames/bind"
 import {
     DownOutlined,
     UserOutlined,
     LogoutOutlined,
     LockFilled,
-} from "@ant-design/icons";
-import style from "../Admin/AdminLayout.module.scss";
-import React from "react";
-import MenuSider from "../Component/MenuSider/MenuSider";
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+} from "@ant-design/icons"
+import style from "../Admin/AdminLayout.module.scss"
+import React, {useState} from "react"
+import MenuSider from "../Component/MenuSider/MenuSider"
 
-const { Header, Content, Sider } = Layout;
-const cx = classname.bind(style);
+const {Header, Content, Sider} = Layout
+const cx = classname.bind(style)
 const items = [
-    { label: "Tài khoản của tôi", key: "0", icon: <UserOutlined /> },
-    { label: "Đổi mật khẩu", key: "1", icon: <LockFilled /> },
-    { label: "Đăng xuất", key: "2", icon: <LogoutOutlined /> },
-];
-function AdminLayout({ children }) {
+    {label: "Tài khoản của tôi", key: "0", icon: <UserOutlined />},
+    {label: "Đổi mật khẩu", key: "1", icon: <LockFilled />},
+    {label: "Đăng xuất", key: "2", icon: <LogoutOutlined />},
+]
+function AdminLayout({children}) {
+    const [collapsed, setCollapsed] = useState(false)
+    const {
+        token: {colorBgContainer, borderRadiusLG},
+    } = theme.useToken()
     return (
-        <Layout>
-            <Sider className={cx("sider")}>
+        <Layout className={cx("my-css")}>
+            <Sider
+                width={"17.5rem"}
+                collapsedWidth='0'
+                onBreakpoint={(broken) => {
+                    console.log(broken)
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type)
+                }}
+                className={cx("sider")}
+            >
                 <div className={cx("sider-box")}>
                     <div className={cx("sider-user")}>
-                        <Dropdown
-                            menu={{ items }}
-                            trigger={["click"]}
-                            placement='bottomRight'
-                        >
+                        <Dropdown menu={{items}} trigger={["click"]} placement='bottomRight'>
                             <Space align='center'>
                                 <Avatar size={45} icon={<UserOutlined />} />
                                 <div className={cx("user-info")}>
                                     <div className={cx("user-content")}>
-                                        <h5 className={cx("text-decor")}>
-                                            HOÀNG XUÂN LỘC
-                                        </h5>
-                                        <DownOutlined
-                                            className={cx("dropdown-icon")}
-                                        />
+                                        <h5 className={cx("text-decor")}>HOÀNG XUÂN LỘC</h5>
+                                        <DownOutlined className={cx("dropdown-icon")} />
                                     </div>
-                                    <span className={cx("text-decor")}>
-                                        System Manager
-                                    </span>
+                                    <span className={cx("text-decor")}>System Manager</span>
                                 </div>
                             </Space>
                         </Dropdown>
@@ -50,30 +56,16 @@ function AdminLayout({ children }) {
                 <MenuSider />
             </Sider>
             <Layout>
-                <Header style={{ height: "76px" }} className={cx("header")}>
-                    <div className={cx("header-children")}>Hello</div>
+                <Header style={{height: "76px"}} className={cx("header")}>
+                    <div className={cx("header-children")}></div>
                 </Header>
                 <Content className={cx("content")}>
                     <div className={cx("content-children")}>{children}</div>
                     <p>long content</p>
-                    {
-                        // indicates very long content
-                        Array.from(
-                            {
-                                length: 100,
-                            },
-                            (_, index) => (
-                                <React.Fragment key={index}>
-                                    {index % 20 === 0 && index ? "more" : "..."}
-                                    <br />
-                                </React.Fragment>
-                            )
-                        )
-                    }
                 </Content>
             </Layout>
         </Layout>
-    );
+    )
 }
 
-export default AdminLayout;
+export default AdminLayout
