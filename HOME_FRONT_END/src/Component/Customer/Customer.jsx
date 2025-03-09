5980cf70c4c5cd7863fe4826117c39137b30d08a
Xuan-Loc <xuanloc060393@gmail.com>
2025-03-09 17:40:58 +0700

    09032025 Start 2

--- HOME_FRONT_END/src/Component/Customer/Customer.jsx
+++ HOME_FRONT_END/src/Component/Customer/Customer.jsx
@@ -1,20 +1,30 @@
 import {Menu, Tabs} from "antd"
+import classname from "classnames/bind"
+import style from "../Customer/Customer.module.scss"
+
+const cx = classname.bind(style)
+const item = [
+    {label: "Thông tin cá nhân", children: "Thông tin cá nhân", key: "1"},
+    {label: "Thay đổi mật khẩu", children: "Thay đổi mật khẩu", key: "2"},
+    {label: "Thông tin khác", children: "Thông tin khác", key: "3"},
+]
 
 function Customer() {
     return (
         <>
             <Tabs
                 tabPosition={"left"}
-                items={Array.from({length: 3}).map((_, i) => {
-                    const id = String(i + 1)
-                    return {
-                        label: `Tab ${id}`,
-                        key: id,
-                        children: `Content of Tab ${id}`,
-                    }
-                })}
-                renderTabBar={(props) => <Menu {...props} style={{color: "red"}} />}
-            />
+                className={cx("tabs-customer")}
+                type='line'
+                tabBarGutter={20}
+                size='large'
+            >
+                {item.map((tab) => (
+                    <Tabs.TabPane key={tab.key} tab={tab.label} className={cx("tabs-panel")}>
+                        {tab.children}
+                    </Tabs.TabPane>
+                ))}
+            </Tabs>
         </>
     )
 }
