83499a5c8c7f0c19c1e369da80fd21461293129a
locday123 <xuanloc060393@gmail.com>
2025-03-14 00:36:36 +0700

    14032025 Start 1

--- HOME_FRONT_END/src/Component/Customer/MyContract/ContractInfo.jsx
+++ HOME_FRONT_END/src/Component/Customer/MyContract/ContractInfo.jsx
@@ -1,19 +1,19 @@
-import {Button, Card, Col, Collapse, Row, Table, Tag} from "antd"
+import { Button, Card, Col, Collapse, Row, Table, Tag } from "antd";
 
 const ContractInfo = (info) => {
     return (
-        <Row gutter={40} style={{rowGap: "10px"}}>
+        <Row gutter={40} style={{ rowGap: "10px" }}>
             {info.map((value, key) => (
-                <Col xs={24} md={8} key={key}>
-                    <Tag color='#108ee9' bordered style={{fontSize: "16px"}}>
+                <Col xs={24} md={12} xl={12} xxl={8} key={key}>
+                    <Tag color='#108ee9' bordered style={{ fontSize: "16px" }}>
                         {value.label}
                     </Tag>
                     {value.name}
                 </Col>
             ))}
         </Row>
-    )
-}
+    );
+};
 
 const ExpensesColumn = [
     {
@@ -46,22 +46,24 @@ const ExpensesColumn = [
         key: "total",
         align: "center",
         render: (_, value) =>
-            ((value.newIndex - value.oldIndex) * value.price).toLocaleString("vi"),
+            ((value.newIndex - value.oldIndex) * value.price).toLocaleString(
+                "vi"
+            ),
     },
-]
+];
 
 const ExpensesTable = (children) => {
-    console.log(children.bill_Detail)
+    console.log(children.bill_Detail);
     return (
         <Table
             pagination={false}
             dataSource={children.bill_Detail}
             columns={ExpensesColumn}
             summary={(pagedata) => {
-                let total = 0
-                pagedata.forEach(({oldIndex, newIndex, price}) => {
-                    total += (newIndex - oldIndex) * price
-                })
+                let total = 0;
+                pagedata.forEach(({ oldIndex, newIndex, price }) => {
+                    total += (newIndex - oldIndex) * price;
+                });
 
                 return (
                     <Table.Summary.Row>
@@ -71,14 +73,18 @@ const ExpensesTable = (children) => {
                         <Table.Summary.Cell index={1}></Table.Summary.Cell>
                         <Table.Summary.Cell index={2}></Table.Summary.Cell>
                         <Table.Summary.Cell index={4}></Table.Summary.Cell>
-                        <Table.Summary.Cell index={5} align='center' colSpan={5}>
+                        <Table.Summary.Cell
+                            index={5}
+                            align='center'
+                            colSpan={5}
+                        >
                             <b>{total.toLocaleString("vi")}</b>
                         </Table.Summary.Cell>
                     </Table.Summary.Row>
-                )
+                );
             }}
         />
-    )
-}
+    );
+};
 
-export {ContractInfo, ExpensesTable}
+export { ContractInfo, ExpensesTable };
--- HOME_FRONT_END/src/Component/Customer/MyContract/MyContract.jsx
+++ HOME_FRONT_END/src/Component/Customer/MyContract/MyContract.jsx
@@ -1,35 +1,62 @@
-import {Button, Card, Col, Collapse, Row, Table, Tag} from "antd"
-import {EditOutlined, SearchOutlined, EllipsisOutlined, SettingOutlined} from "@ant-design/icons"
-import {ContractInfo, ExpensesTable} from "./ContractInfo"
+import { Button, Card, Col, Collapse, Row, Table, Tag } from "antd";
+import {
+    EditOutlined,
+    SearchOutlined,
+    EllipsisOutlined,
+    SettingOutlined,
+    CheckCircleFilled,
+    SyncOutlined,
+} from "@ant-design/icons";
+import { ContractInfo, ExpensesTable } from "./ContractInfo";
+const mang = [];
 
 const contractData = [
     {
         id_rent: "HDS-26022025-001",
         contract_rentInfo: [
-            {label: "Người thuê:", name: "Hoàng Xuân Lộc", key: "1"},
-            {label: "CCCD", name: "197322197", key: "2"},
-            {label: "Số điện thoại", name: "0374536393", key: "3"},
-            {label: "Phí hợp đồng", name: 0, key: "4"},
-            {label: "Hiệu lực", name: "12/03/20225 - 12/03/2026", key: "5"},
+            { label: "Người thuê:", name: "Hoàng Xuân Lộc", key: "1" },
+            { label: "CCCD", name: "197322197", key: "2" },
+            { label: "Số điện thoại", name: "0374536393", key: "3" },
+            { label: "Phí hợp đồng", name: 0, key: "4" },
+            { label: "Hiệu lực", name: "12/03/20225 - 12/03/2026", key: "5" },
         ],
 
         home_rentInfo: [
-            {label: "Tòa nhà:", name: "672 Lê Hồng Phong", key: "1"},
-            {label: "Tầng", name: "4", key: "2"},
-            {label: "Phòng", name: "A401", key: "3"},
-            {label: "Số người ở", name: 2, key: "8"},
-            {label: "Phí thuê", name: (10000000).toLocaleString("vi"), key: "4"},
-            {label: "Phí cọc", name: (10000000).toLocaleString("vi"), key: "5"},
-            {label: "Phí điện", name: (4000).toLocaleString("vi"), key: "6"},
-            {label: "Phí nước", name: (100000).toLocaleString("vi"), key: "7"},
+            { label: "Tòa nhà:", name: "672 Lê Hồng Phong", key: "1" },
+            { label: "Tầng", name: "4", key: "2" },
+            { label: "Phòng", name: "A401", key: "3" },
+            { label: "Số người ở", name: 2, key: "8" },
+            {
+                label: "Phí thuê",
+                name: (10000000).toLocaleString("vi"),
+                key: "4",
+            },
+            {
+                label: "Phí cọc",
+                name: (10000000).toLocaleString("vi"),
+                key: "5",
+            },
+            { label: "Phí điện", name: (4000).toLocaleString("vi"), key: "6" },
+            {
+                label: "Phí nước",
+                name: (100000).toLocaleString("vi"),
+                key: "7",
+            },
 
-            {label: "Tình trạng", name: "ĐÃ KÍCH HOẠT", key: "9"},
+            { label: "Tình trạng", name: "ĐÃ KÍCH HOẠT", key: "9" },
         ],
         bill_HomeRent: [
             {
                 label: "CHI PHÍ THUÊ NHÀ THÁNG 1",
+                statusBill: true,
                 bill_Detail: [
-                    {key: "1", Expenses: "Tiền nhà", oldIndex: 0, newIndex: 1, price: 10000000},
+                    {
+                        key: "1",
+                        Expenses: "Tiền nhà",
+                        oldIndex: 0,
+                        newIndex: 1,
+                        price: 10000000,
+                    },
                     {
                         key: "1",
                         Expenses: "Chi phí sử dụng điệaaan",
@@ -55,11 +82,18 @@ const contractData = [
             },
             {
                 label: "CHI PHÍ THUÊ NHÀ THÁNG 2",
+                statusBill: false,
                 bill_Detail: [
-                    {key: "1", Expenses: "Tiền nhà", oldIndex: 0, newIndex: 1, price: 10000000},
                     {
                         key: "1",
-                        Expenses: "Chi phí sử dụng điện",
+                        Expenses: "Tiền nhà",
+                        oldIndex: 0,
+                        newIndex: 1,
+                        price: 10000000,
+                    },
+                    {
+                        key: "1",
+                        Expenses: "Chi phí sử dụng điệaaan",
                         oldIndex: 100,
                         newIndex: 200,
                         price: 4000,
@@ -82,45 +116,70 @@ const contractData = [
             },
         ],
     },
-]
+];
 
 function MyContract() {
     const CollapseData = (children) => [
         {
             key: "1",
-            label: <b style={{color: "white"}}>THÔNG TIN HỢP ĐỒNG</b>,
+            label: <b style={{ color: "white" }}>THÔNG TIN HỢP ĐỒNG</b>,
             children: ContractInfo(children.contract_rentInfo),
         },
         {
             key: "2",
-            label: <b style={{color: "white"}}>THÔNG TIN CĂN HỘ</b>,
+            label: <b style={{ color: "white" }}>THÔNG TIN CĂN HỘ</b>,
             children: ContractInfo(children.home_rentInfo),
         },
         {
             key: "3",
-            label: <b style={{color: "white"}}>HÓA ĐƠN TIỀN NHÀ</b>,
+            label: <b style={{ color: "white" }}>HÓA ĐƠN TIỀN NHÀ</b>,
             children: (
                 <Collapse
                     size='small'
-                    {...children.bill_HomeRent.map((value) => {
-                        item: {
-                            label: value.label
-                        }
+                    items={children.bill_HomeRent.map((value) => {
+                        return {
+                            label: value.statusBill ? (
+                                <Tag
+                                    bordered={false}
+                                    style={{
+                                        fontSize: "14px",
+                                        fontWeight: "bolder",
+                                    }}
+                                    icon={<CheckCircleFilled />}
+                                    color='success'
+                                >
+                                    {value.label + " -  ĐÃ THANH TOÁN"}
+                                </Tag>
+                            ) : (
+                                <Tag
+                                    bordered={false}
+                                    style={{
+                                        fontSize: "14px",
+                                        fontWeight: "bolder",
+                                    }}
+                                    icon={<SyncOutlined spin />}
+                                    color='processing'
+                                >
+                                    {value.label + " -  CHƯA THANH TOÁN"}
+                                </Tag>
+                            ),
+                            children: ExpensesTable(value),
+                        };
                     })}
                 />
             ),
         },
-    ]
+    ];
 
     return (
-        <Row style={{rowGap: "20px"}}>
+        <Row style={{ rowGap: "20px" }}>
             {contractData.map((value, key) => (
                 <Col span={24} key={key}>
                     <Card
                         size='small'
                         variant='outlined'
                         title={
-                            <Tag bordered style={{fontSize: "16px"}}>
+                            <Tag bordered style={{ fontSize: "16px" }}>
                                 {value.id_rent}
                             </Tag>
                         }
@@ -129,7 +188,7 @@ function MyContract() {
                         }}
                     >
                         <Collapse
-                            style={{backgroundColor: "#fda849"}}
+                            style={{ backgroundColor: "#fda849" }}
                             size='small'
                             items={CollapseData(value)}
                         />
@@ -137,7 +196,7 @@ function MyContract() {
                 </Col>
             ))}
         </Row>
-    )
+    );
 }
 
-export default MyContract
+export default MyContract;
