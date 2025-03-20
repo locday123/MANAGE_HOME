7129369ac39cd99217cbafb34b8e75b485dadd2b
Xuan-Loc <xuanloc060393@gmail.com>
2025-03-20 18:33:34 +0700

    20032025 Commit 2

--- HOME_FRONT_END/src/Component/Home/Home.jsx
+++ HOME_FRONT_END/src/Component/Home/Home.jsx
@@ -1,6 +1,29 @@
-import {Table} from "antd"
+import {Card, Table} from "antd"
+import {useEffect, useState} from "react"
+
+import {getAllHome} from "../../Service/Home//HomeSerivce"
+import {columnsTable} from "./HomeExtend"
 
 function Home() {
-    return <Table />
+    const [home, setHome] = useState([])
+    useEffect(() => {
+        getAllHome().then((value) => {
+            setHome(value)
+        })
+    })
+
+    return (
+        <Card>
+            <Table
+                columns={columnsTable}
+                pagination={false}
+                scroll={{
+                    x: "max-content",
+                    y: 80 * 5,
+                }}
+                dataSource={home}
+            />
+        </Card>
+    )
 }
 export default Home
