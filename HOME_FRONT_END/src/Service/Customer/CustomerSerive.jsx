8ef8870361e16c19f556323c55dc4d69a0fa6f73
Xuan-Loc <xuanloc060393@gmail.com>
2025-03-25 19:49:05 +0700

    25/03/2025 Comit 1

--- HOME_FRONT_END/src/Service/Customer/CustomerSerive.jsx
+++ HOME_FRONT_END/src/Service/Customer/CustomerSerive.jsx
@@ -1,14 +1,14 @@
-import { client } from "../Service";
-
-let linkApi = "https://mocki.io/v1/d7bb89a9-1e54-4dd9-ac9b-8edd276b4903";
+import {client} from "../Service"
+;``
+let linkApi = "https://mocki.io/v1/d7bb89a9-1e54-4dd9-ac9b-8edd276b4903"
 
 const getAllCustomer = async () => {
     try {
-        const response = await client.get(linkApi);
-        return await response.data;
+        const response = await client.get(linkApi)
+        return await response.data
     } catch (err) {
-        console.log(err);
+        console.log(err)
     }
-};
+}
 
-export { getAllCustomer };
+export {getAllCustomer}
