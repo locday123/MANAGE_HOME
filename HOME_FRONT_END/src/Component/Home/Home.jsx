47f435ab130ef3a8955d2097c2b73569c0744b5c
locday123 <xuanloc060393@gmail.com>
2025-04-03 23:05:15 +0700

    3/4/2025 comit 2

--- HOME_FRONT_END/src/Component/Home/Home.jsx
+++ HOME_FRONT_END/src/Component/Home/Home.jsx
@@ -1,237 +1,121 @@
-<<<<<<< HEAD
-import {
-    Card,
-    Checkbox,
-    Col,
-    Dropdown,
-    Menu,
-    Modal,
-    Row,
-    Segmented,
-    Space,
-    Table,
-} from "antd";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { Card, Col, Grid, Row, Segmented, Table, Tag } from "antd";
+import { useEffect, useState } from "react";
+
 import { AppstoreOutlined, BarsOutlined } from "@ant-design/icons";
-import { getAllHome } from "../../Service/Home//HomeSerivce";
-import { columnsTable, FormFilter, actionsCard } from "./HomeExtend";
-import HomeModal from "./HomeModal";
+import {
+    SearchBar,
+    columnsTable,
+    NewUsersCard,
+    actionsCard,
+} from "../Customer/CustomerExtend";
+import { getAllCustomer } from "../../Service/Customer/CustomerSerive";
 
-function Home() {
-    const [home, setHome] = useState([]);
-    const [searchText, setSearchText] = useState("");
-    const [visible, setVisible] = useState(false);
-    const [isGridView, setIsGridView] = useState("List");
-    const [dropdownOpen, setDropdownOpen] = useState(false);
+function Customer() {
+    const [data, setData] = useState([]);
     const [hoveredCard, setHoveredCard] = useState(null);
-=======
-import {Card, Checkbox, Col, Dropdown, Menu, Row, Segmented, Space, Table} from "antd"
-import {useEffect, useMemo, useRef, useState} from "react"
-import {AppstoreOutlined, BarsOutlined} from "@ant-design/icons"
+    const [searchText, setSearchText] = useState("");
+    const [isGridView, setIsGridView] = useState(false);
+    const [visible, setVisible] = useState(false);
+    const [customerData, setCustomerData] = useState({});
 
-import {getAllHome} from "../../Service/Home//HomeSerivce"
-import {columnsTable, FormFilter} from "./HomeExtend"
-import {getAllFloor, searchHomeInFloor} from "../../Service/FLOOR/FloorService"
+    const filteredData = data.filter((item) => {
+        const filterName = item.customer_Name
+            .toLowerCase()
+            .includes(searchText.toLowerCase());
+        const filterID = item.customer_ID.toString().includes(searchText);
+        const filterPhoneNumber = item.customer_PhoneNumber
+            .toString()
+            .includes(searchText);
+        return filterName || filterID || filterPhoneNumber;
+    }); // Tìm kiếm Customer
 
-function Home() {
-    const [home, setHome] = useState([])
-    const [searchText, setSearchText] = useState("")
-    const [isGridView, setIsGridView] = useState("List")
-    const [dropdownOpen, setDropdownOpen] = useState(false)
->>>>>>> a6cee632a6f58cb185729ab66ba05b01927efcf1
-
-    const [selectedValues, setSelectedValues] = useState([
-        "home_ID",
-        "home_Address",
-        "home_RentalPrice",
-        "contract",
-        "home_Status",
-        "action",
-<<<<<<< HEAD
-        "home_TotalFloors",
-    ]);
-=======
-    ])
->>>>>>> a6cee632a6f58cb185729ab66ba05b01927efcf1
-
-    const filteredData = home
-        ? home.filter(
-              (item) =>
-                  item.home_ID?.toLowerCase().includes(searchText.toLowerCase()) ||
-                  item.home_Address?.toLowerCase().includes(searchText.toLowerCase())
-          )
-        : []
-    const allSelected = selectedValues.length === columnsTable.length
-    const handleSelectAll = (event) => {
-        event.stopPropagation() // Ngăn dropdown bị đóng khi click
-        if (allSelected) {
-            setSelectedValues([]) // Bỏ chọn tất cả
-        } else {
-            setSelectedValues(columnsTable.map(({key}) => key)) // Chọn tất cả
-        }
-    }
-    const handleCheckboxChange = (key, event) => {
-        event.stopPropagation() // Ngăn dropdown bị đóng khi click
-        setSelectedValues((prev) =>
-            prev.includes(key) ? prev.filter((item) => item !== key) : [...prev, key]
-        )
-        setDropdownOpen(true) // Giữ dropdown mở
-    }
-
-    const items = useMemo(() => {
-        return columnsTable.map(({key, title}) => ({
-            key: key,
-            label: (
-                <div onClick={(e) => e.stopPropagation()} style={{padding: "0px 10px"}}>
-                    <Checkbox
-                        checked={selectedValues.includes(key)}
-                        onChange={(event) => handleCheckboxChange(key, event)}
-                    >
-                        {title}
-                    </Checkbox>
-                </div>
-            ),
-        }))
-    }, [columnsTable, selectedValues])
-    const menu = <Menu items={items} />
-    const newColumns = columnsTable.map((item) =>
-        Object.assign(Object.assign({}, item), {
-            hidden: !selectedValues.includes(item.key),
-        })
-    )
-
-    const handleSearchChange = (e) => {
-        setSearchText(e.target.value)
-    }
     useEffect(() => {
-        getAllHome().then((value) => {
-<<<<<<< HEAD
-            setHome(value);
+        getAllCustomer().then((value) => {
+            setData(value);
         });
     }, []);
     return (
-        <Card
-            title={
-                <FormFilter
-                    searchText={searchText}
-                    onChange={handleSearchChange}
-                    setVisible={setVisible}
-                    visible={visible}
-                />
-            }
-            extra={
-                <Space size={"large"}>
-                    {isGridView ? (
-                        <Space
-                            size={"small"}
-                            style={{
-                                border: "1px solid #ddd",
-                                padding: "6px",
-                                borderRadius: "8px",
-                            }}
-=======
-            setHome(value)
-        })
-    }, [home])
-    return (
-        <Card
-            title={<FormFilter searchText={searchText} onChange={handleSearchChange} />}
-            extra={
-                <Space size={"large"}>
-                    <Space
-                        size={"small"}
-                        style={{
-                            border: "1px solid #ddd",
-                            padding: "6px",
-                            borderRadius: "8px",
-                        }}
-                    >
-                        <Checkbox
-                            checked={allSelected}
-                            onChange={(event) => handleSelectAll(event)}
-                        />
-                        <Dropdown
-                            open={dropdownOpen} // Kiểm soát trạng thái dropdown
-                            onOpenChange={setDropdownOpen} // Cập nhật trạng thái khi click ngoài
-                            overlay={menu}
-                            trigger={["click"]}
-                            placement={"bottom"}
-                            arrow={{pointAtCenter: true}}
->>>>>>> a6cee632a6f58cb185729ab66ba05b01927efcf1
-                        >
-                            <Checkbox
-                                checked={allSelected}
-                                onChange={(event) => handleSelectAll(event)}
-                            />
-                            <Dropdown
-                                open={dropdownOpen} // Kiểm soát trạng thái dropdown
-                                onOpenChange={setDropdownOpen} // Cập nhật trạng thái khi click ngoài
-                                overlay={menu}
-                                trigger={["click"]}
-                                placement={"bottom"}
-                                arrow={{ pointAtCenter: true }}
-                            >
-                                <span>Hiển thị {selectedValues.length}</span>
-                            </Dropdown>
-                        </Space>
-                    ) : null}
-
-                    <Segmented
-                        onChange={(value) => setIsGridView(value === "List")}
-                        options={[
-                            {value: "List", icon: <BarsOutlined />},
-                            {value: "Grid", icon: <AppstoreOutlined />},
-                        ]}
-                        style={{marginRight: "16px"}}
+        <>
+            <NewUsersCard data={data} />
+            <Card
+                title={
+                    <SearchBar
+                        searchText={searchText}
+                        setSearchText={setSearchText}
+                        setData={setData}
+                        visible={visible}
+                        setVisible={setVisible}
+                        customerData={customerData}
+                        setCustomerData={setCustomerData}
                     />
-                </Space>
-            }
-        >
-            {isGridView ? (
-                <Table
-                    columns={newColumns}
-                    pagination={false}
-                    scroll={{
-                        x: "max-content",
-                        y: 80 * 5,
-                    }}
-                    dataSource={filteredData}
-                    rowKey='home_ID'
-                />
-            ) : (
-                <Row gutter={[10, 10]}>
-                    {filteredData.map((item) => (
-                        <Col xs={24} sm={12} md={8} lg={6} key={item.home_ID}>
-                            <Card
-                                onMouseEnter={() =>
-                                    setHoveredCard(item.home_ID)
-                                }
-                                onMouseLeave={() => setHoveredCard(null)} // Reset khi rời chuột
-                                hoverable
-                                title={item.home_ID}
-                                extra={
-                                    hoveredCard === item?.home_ID &&
-                                    actionsCard()
-                                }
-                            >
-                                <p>
-                                    <b>Địa chỉ:</b> {item.home_Address}
-                                </p>
-                                <p>
-                                    <b>Giá thuê:</b> {item.home_RentalPrice.toLocaleString()} VND
-                                </p>
-                                <p>
-                                    <b>Chủ nhà:</b> {item.home_HostName}
-                                </p>
-                                <p>
-                                    <b>SĐT:</b> {item.home_HostPhoneNumber}
-                                </p>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
-            )}
-        </Card>
-    )
+                }
+                extra={
+                    <Segmented
+                        onChange={(value) => setIsGridView(value === "Grid")}
+                        options={[
+                            { value: "List", icon: <BarsOutlined /> },
+                            { value: "Grid", icon: <AppstoreOutlined /> },
+                        ]}
+                        style={{ marginRight: "16px" }}
+                    />
+                }
+                style={{
+                    boxShadow: "rgba(0, 0, 0, 0.03) 0px 0px 5px 5px",
+                }}
+            >
+                <div style={{ maxHeight: "36.1rem", overflowY: "auto" }}>
+                    {isGridView ? (
+                        <div style={{ padding: "16px" }}>
+                            <Row gutter={[16, 16]}>
+                                {filteredData.map((customer) => (
+                                    <Col
+                                        xs={24}
+                                        sm={12}
+                                        md={8}
+                                        lg={8}
+                                        xl={8}
+                                        xxl={6}
+                                        key={customer.customer_ID}
+                                    >
+                                        <Card
+                                            hoverable
+                                            onMouseEnter={() =>
+                                                setHoveredCard(
+                                                    customer.customer_ID
+                                                )
+                                            } // Lưu ID của card đang hover
+                                            onMouseLeave={() =>
+                                                setHoveredCard(null)
+                                            } // Reset khi rời chuột
+                                            onClick={() =>
+                                                setCustomerData(customer)
+                                            }
+                                            title={customer.customer_Name}
+                                            extra={
+                                                hoveredCard ===
+                                                    customer?.customer_ID &&
+                                                actionsCard(setVisible)
+                                            } // Chỉ hiển thị actions cho card đang hover
+                                        ></Card>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </div>
+                    ) : (
+                        <Table
+                            columns={columnsTable(setData, setVisible)}
+                            dataSource={filteredData}
+                            pagination={false} // Ẩn pagination mặc định
+                            onRow={(record) => ({
+                                onClick: () => {
+                                    setCustomerData(record); // Gán dữ liệu vào form
+                                },
+                            })}
+                        />
+                    )}
+                </div>
+            </Card>
+        </>
+    );
 }
-export default Home
+export default Customer;
