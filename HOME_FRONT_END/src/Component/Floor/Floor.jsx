4dd25b4198109571454001103526b5cd9377ed81
locday123 <xuanloc060393@gmail.com>
2025-04-16 00:17:04 +0700

    c

--- HOME_FRONT_END/src/Component/Floor/Floor.jsx
+++ HOME_FRONT_END/src/Component/Floor/Floor.jsx
@@ -1,34 +1,58 @@
-import {Button, Card, Tabs} from "antd"
-import style from "../Floor/Floor.module.scss"
-import classname from "classnames/bind"
-import {getAllHome} from "../../Service/Home/HomeSerivce"
-import {useEffect, useState} from "react"
-import {CloseCircleTwoTone, CheckCircleTwoTone, AppstoreAddOutlined} from "@ant-design/icons"
-import CustomCard from "../Extend/Card/CustomCard"
-import ListFloor from "./ListFloor"
-import {FormFilter} from "../Floor/FloorExtend"
-const cx = classname.bind(style)
+import { Button, Card, Table, Tabs } from "antd";
+import style from "../Floor/Floor.module.scss";
+import classname from "classnames/bind";
+import { getAllHome } from "../../Service/Home/HomeSerivce";
+import { useEffect, useState } from "react";
+import { AppstoreAddOutlined } from "@ant-design/icons";
+import CustomCard from "../Extend/Card/CustomCard";
+
+import { FormFilter, columnsTable } from "../Floor/FloorExtend";
+const cx = classname.bind(style);
 function Floor() {
-    const [home, setHome] = useState([])
-    const [valueCreate, setValueCreate] = useState([])
+    const [home, setHome] = useState([]);
+    const [floors, setFloors] = useState([]);
+    const [valueCreate, setValueCreate] = useState([]);
     useEffect(() => {
         getAllHome().then((data) => {
-            setHome(data)
-        })
-    }, [])
+            setHome(data);
+        });
+    }, []);
+    const sortedFloors = [...floors].sort((a, b) => {
+        const getFloorIndex = (floorName) => {
+            if (!floorName) return Infinity;
+            if (floorName.toLowerCase().includes("Trệt")) return 0;
 
+            const match = floorName.match(/\d+/);
+            return match ? parseInt(match[0], 10) : Infinity;
+        };
+
+        return getFloorIndex(a.floor_Name) - getFloorIndex(b.floor_Name);
+    });
     return (
         <CustomCard
             title={
-                <FormFilter data={home} valueCreate={valueCreate} setValueCreate={setValueCreate} />
+                <FormFilter
+                    data={home}
+                    valueCreate={valueCreate}
+                    setValueCreate={setValueCreate}
+                    setFloors={setFloors}
+                />
             }
             extra={
-                <Button icon={<AppstoreAddOutlined style={{fontSize: "22px"}} />} type='primary' />
+                <Button
+                    icon={<AppstoreAddOutlined style={{ fontSize: "22px" }} />}
+                    type='primary'
+                />
             }
         >
-            {valueCreate && valueCreate.home_ID}
+            <Table
+                showHeader={false}
+                dataSource={floors}
+                columns={columnsTable}
+                pagination={false}
+            />
         </CustomCard>
-    )
+    );
 }
 
-export default Floor
+export default Floor;
--- HOME_FRONT_END/src/Component/Floor/FloorExtend.jsx
+++ HOME_FRONT_END/src/Component/Floor/FloorExtend.jsx
@@ -1,22 +1,89 @@
-import {Button, Col, Input, Row, Select} from "antd"
-import {createFloorsForHome} from "../../Service/Floor/FloorService"
-import {useState} from "react"
+import { Button, Col, Input, Row, Select, Tag } from "antd";
+import {
+    createFloorsForHome,
+    getFloorsByHome,
+} from "../../Service/Floor/FloorService";
+import { useState } from "react";
 
-const FormFilter = ({searchText, data = [], valueCreate, setValueCreate}) => {
-    const [message, setMessage] = useState("")
+const columnsTable = [
+    {
+        key: "floor_ID",
+        title: "ID",
+        dataIndex: "floor_ID",
+        align: "center",
+        width: "5rem",
+
+        render: (value) => <Tag>{value}</Tag>,
+    },
+    {
+        key: "home_ID",
+        title: "Home",
+        dataIndex: "home_ID",
+        align: "center",
+        width: "5rem",
+
+        render: (value) => <Tag>{value}</Tag>,
+    },
+    {
+        key: "floor_Name",
+        title: "Tầng",
+        dataIndex: "floor_Name",
+        align: "center",
+        width: "5rem",
+
+        render: (value) => <Tag>{value}</Tag>,
+    },
+    {
+        key: "floor_TotalRooms",
+        title: "Tầng",
+        dataIndex: "floor_TotalRooms",
+        align: "center",
+        width: "5rem",
+
+        render: (value) => <Tag>{value}</Tag>,
+    },
+    {
+        key: "created_at",
+        title: "Tầng",
+        dataIndex: "created_at",
+        align: "center",
+        width: "5rem",
+
+        render: (value) => <Tag>{value}</Tag>,
+    },
+];
+const FormFilter = ({
+    searchText,
+    data = [],
+    valueCreate,
+    setValueCreate,
+    setFloors,
+}) => {
+    const [message, setMessage] = useState("");
 
     const handleCreateFloors = async (valueCreate) => {
-        const result = await createFloorsForHome(valueCreate.home_ID, valueCreate.home_TotalFloors)
+        const result = await createFloorsForHome(
+            valueCreate.home_ID,
+            valueCreate.home_TotalFloors
+        );
 
         if (result?.data) {
-            setMessage(`✅ Tạo ${result.data.length} tầng thành công.`)
+            setMessage(`✅ Tạo ${result.data.length} tầng thành công.`);
         } else {
-            setMessage(result.error || "❌ Không thể tạo tầng.")
+            setMessage(result.error || "❌ Không thể tạo tầng.");
         }
-    }
+    };
+    const handleChangeHome = async (home_ID) => {
+        try {
+            const result = await getFloorsByHome(home_ID);
+            setFloors(result || []); // nếu API trả về { data: [...] }
+        } catch (error) {
+            console.error("Lỗi khi lấy danh sách tầng:", error);
+        }
+    };
     return (
         <>
-            <Row gutter={[24, 24]} style={{rowGap: "10px"}}>
+            <Row gutter={[24, 24]} style={{ rowGap: "10px" }}>
                 <Col xxl={4} xl={6} lg={8}>
                     <Input
                         allowClear
@@ -30,9 +97,13 @@ const FormFilter = ({searchText, data = [], valueCreate, setValueCreate}) => {
                 <Col>
                     <Select
                         placeholder='Quận - Huyện'
-                        onChange={(value, option) =>
-                            setValueCreate({home_ID: value, home_TotalFloors: option.totalFloors})
-                        }
+                        onChange={(value, option) => {
+                            handleChangeHome(value);
+                            setValueCreate({
+                                home_ID: value,
+                                home_TotalFloors: option.totalFloors,
+                            });
+                        }}
                         options={data.map((home) => ({
                             key: home.home_ID,
                             value: home.home_ID,
@@ -44,13 +115,13 @@ const FormFilter = ({searchText, data = [], valueCreate, setValueCreate}) => {
                 <Col>
                     <Button
                         type='primary'
-                        children='Tự động thêm tầng'
+                        children='Tự động tạo tầng'
                         onClick={() => handleCreateFloors(valueCreate)}
                     />
                 </Col>
             </Row>
         </>
-    )
-}
+    );
+};
 
-export {FormFilter}
+export { FormFilter, columnsTable };
